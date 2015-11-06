1913eebab1a52d0854f34e4a04ae66f5dabfa99e
Jorge García Rojo <jgr3009@gmail.com>
2015-11-06 17:59:24 +0100

    Inicio del proyecto
    Solo tiene una clase que pone un mensaje en la pantalla

---
+++ src/Ejercicio1.java
@@ -0,0 +1,21 @@
+/*
+ * Autor: Jorge García Rojo
+ * 
+ * Ejemplo de uso de GitHub
+ * 
+ */
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.program.*;
+
+
+public class Ejercicio1 extends GraphicsProgram{
+	
+	public void run(){
+		
+		GLabel etiqueta = new GLabel ("HOLA GIT");
+		add(etiqueta, 120,130);
+	}
+		
+}
