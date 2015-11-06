f54082b091daa4ddaf0d4d501b32effcf2b86900
Jorge García Rojo <jgr3009@gmail.com>
2015-11-06 20:12:25 +0100

    Finalizada la piramide
    Falta girarla

--- src/Ejercicio1.java
+++ src/Ejercicio1.java
@@ -11,11 +11,31 @@ import acm.program.*;
 
 
 public class Ejercicio1 extends GraphicsProgram{
-	
+
 	public void run(){
-		
-		GLabel etiqueta = new GLabel ("HOLA GIT");
-		add(etiqueta, 120,130);
+
+		//cambio el tamaño de la ventana
+		setSize(800,600);
+		//llamo al método que dibuja la piramide
+		pintaPiramide();
 	}
+	public void pintaPiramide(){
+
+		int numeroLadrillosBase = 14;
+		int anchoLadrillo = 30;
+		int altoLadrillo = 12;
 		
+		//querido yo del martes;
+		//te falta lo más facil que es girar la pirámide
+		//att: tu yo del viernes
+
+		for (int j=0; j< numeroLadrillosBase; j++){
+			for (int i=0; i< numeroLadrillosBase-j; i++){
+				GRect ladrillo = new GRect (anchoLadrillo, altoLadrillo);
+				add(ladrillo, anchoLadrillo *j/2 + anchoLadrillo * i, altoLadrillo * j);
+				pause(20);
+			}
+
+		}
+	}
 }
